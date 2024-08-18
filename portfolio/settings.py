d10ba02bf8b550484ffb7c538f47b83a840158f8
Shakib <sakibmondal7@gmail.com>
2024-08-18 22:03:52 +0530

    initial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2dk3%8@km8-sgj%1x-hnbs=7!h7h1uopfb2^2053n$&e6qj$t9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
