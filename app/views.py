927784b7a3bace3f3210affcedb2222323fd62cd
Shakib Mondal <sakibmondal7@gmail.com>
2025-05-01 18:03:58 +0530

    Email nortification for message is activated.

--- app/views.py
+++ app/views.py
@@ -10,6 +10,16 @@ def home(request):
     if request.method == 'POST':
         message_form = MessageForm(request.POST)
         if message_form.is_valid():
+            name = message_form.cleaned_data['name']
+            email = message_form.cleaned_data['email']
+            subject = message_form.cleaned_data['subject']
+            body = message_form.cleaned_data['body']
+            send_mail(
+                name=name,
+                email=email,
+                subject=subject,
+                body=body
+            )
             message_form.save()
             message_form = MessageForm()
             alert_message = {'color' : 'success' , 'message' : 'Your Message is Successfully Delevered.'}
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os 
+from config import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2dk3%8@km8-sgj%1x-hnbs=7!h7h1uopfb2^2053n$&e6qj$t9'
+SECRET_KEY = Config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
